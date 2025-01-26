92ea889322215f40131574cd3b75c190fd9ca0f6
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2025-01-26 17:03:04 -0300

    warning mobile and db att

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,27 @@ import Highlights from './Highlights.jsx';
 import Formation from './Formation.jsx';
 import Stacks from './Stacks.jsx';
 import Contact from './Contact.jsx';
+import loading from '../img/gif/loading waiting GIF.gif';
 
 
 function Home() {
+
   return (
-    <div>
-      <FrontPage />
-      <About />
-      <Highlights />
-      <Formation />
-      <Stacks />
-      <Contact /> 
+    <div className="">
+      <div className="lg:block max-md:hidden">
+        <FrontPage />
+        <About />
+        <Highlights />
+        <Formation />
+        <Stacks />
+        <Contact /> 
+      </div>
+      <div className="lg:hidden flex flex-col h-screen justify-center items-center space-y-4">
+        <h1 className="text-xl text-main-orange text-center p-2">
+           Versão Mobile Em Contrução...
+        </h1>
+        <img src={loading} alt="loading" />
+      </div>
     </div>
   );
 }
